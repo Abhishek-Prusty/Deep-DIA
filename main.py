98368092e79f1863ef1024076871c664ba6bc95a
Abhishek Prusty <abhishekprusty1234567890@gmail.com>
2018-10-09 00:24:13 +0530

    added data augmentation

--- main.py
+++ main.py
@@ -18,37 +18,82 @@ from itertools import cycle
 import glob
 from keras.preprocessing.image import img_to_array
 from keras.preprocessing.image import load_img
+from keras.preprocessing.image import ImageDataGenerator
 
+
+def fixed_generator(generator):
+    for batch in generator:
+        yield (batch, batch)
 
 INIT_LR=1e-3
-EPOCHS=10
-BATCH_SIZE=256
+EPOCHS=100
+BATCH_SIZE=128
+SIZE=32
+
+
+
+
 autoencoder=model.makeModel()	
 #print(autoencoder.summary())
 
 opt2=RMSprop(lr=INIT_LR, decay=1e-6)
 autoencoder.compile(loss="mean_squared_error", optimizer = opt2)
 
-with open('data1.pickle','rb') as f:
+'''
+with open('data2.pickle','rb') as f:
 	data=pickle.load(f)
 
-with open('labels1.pickle','rb') as f:
+with open('labels2.pickle','rb') as f:
     labels=pickle.load(f)
-
+'''
 #####
+
 files = glob.glob ("Challenge-3-ForTrain/train_image/*.jpg")
 files=sorted(files)
 
 files2 = glob.glob ("Challenge-3-ForTest/test_image_random/*.jpg")
 files2=sorted(files2)
 
-data = np.array(data, dtype="float") / 255.0
+
+#data = np.array(data, dtype="float") / 255.0
 #print(data.shape)
 
 #print(data[:3])
 #print(len(labels))
 
-x_train,x_test,y_train,y_test=train_test_split(data,data,test_size=0.1)
+#x_train,x_test,y_train,y_test=train_test_split(data,data,test_size=0.1)
+
+train_folder="/home/abhishek/DeepDIA/Challenge-3-ForTrain/"
+test_folder="/home/abhishek/DeepDIA/Challenge-3-ForTest/"
+
+train_datagen = ImageDataGenerator(
+        rotation_range=10,
+        width_shift_range=0.1,
+        height_shift_range=0.1,
+        rescale=1./255,
+        shear_range=0.2,
+        zoom_range=0.2,
+        fill_mode='nearest')
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+train_generator = train_datagen.flow_from_directory(
+        train_folder,
+        target_size=(SIZE, SIZE),
+        batch_size=BATCH_SIZE,
+        shuffle=False,
+        color_mode = "grayscale",
+        class_mode=None)
+
+validation_generator = test_datagen.flow_from_directory(
+        test_folder,
+        target_size=(SIZE, SIZE),
+        batch_size=BATCH_SIZE,
+        color_mode = "grayscale",
+        shuffle=False,
+        class_mode=None)
+
+
 
 def next_power_of_2(x):  
     return 1 if x == 0 else 2**(x - 1).bit_length()
@@ -90,21 +135,22 @@ def imageLoader(files, batch_size=1):
             batch_end += batch_size
 
 
-print(len(files))
-print(len(files2))
+#print(len(files))
+#print(len(files2))
+
 
 hist = autoencoder.fit_generator(
-                imageLoader(files,1),
+                fixed_generator(train_generator),
                 epochs=EPOCHS,
-                steps_per_epoch=len(files),
+                steps_per_epoch=len(files)//BATCH_SIZE,
                 verbose=1,
-                validation_data=imageLoader(files2,1),
-                validation_steps=len(files2),
-                callbacks=[TensorBoard(log_dir='/tmp/run23')]
+                validation_data=fixed_generator(validation_generator),
+                validation_steps=len(files2)//BATCH_SIZE,
+                callbacks=[TensorBoard(log_dir='/tmp/run25')]
                 )
-
-
 '''
+
+
 autoencoder.fit(x_train, y_train,
                 epochs=EPOCHS,
                 batch_size=BATCH_SIZE,
@@ -113,6 +159,7 @@ autoencoder.fit(x_train, y_train,
                 callbacks=[TensorBoard(log_dir='/tmp/run20')]
                 )
 '''
+
 name='model-{}'.format(str(datetime.now()))
 autoencoder.save(name+'.h5')
 
@@ -125,13 +172,13 @@ plt.figure(figsize=(20, 4))
 for i in range(1,n):
 
     ax = plt.subplot(2, n, i)
-    plt.imshow(x_test[i])
+    plt.imshow(x_test[i].reshape(SIZE,SIZE))
     plt.gray()
     ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 
     ax = plt.subplot(2, n, i + n)
-    im=decoded_imgs[i]
+    im=decoded_imgs[i].reshape(SIZE,SIZE)
     #ret,thresh_img = cv2.threshold(im,140,255,cv2.THRESH_BINARY)
     plt.imshow(im)
     plt.gray()
